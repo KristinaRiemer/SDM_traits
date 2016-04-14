c614e236f25a13ed268cd5c27d727380e54e03d5
Kristina Riemer <kristina.riemer@weecology.org>
2016-04-14 15:57:57 -0400

    Visualize and prep enviro and occurrence data for MaxEnt

---
+++ SDM_sizes_singlesp.R
@@ -0,0 +1,39 @@
+library(rgdal)
+library(raster)
+library(sp)
+
+# Read in and plot a country basemap
+world = readOGR("global_country_boundaries/", "TM_WORLD_BORDERS_SIMPL-0.3")
+spplot(world, zcol = 1, col.regions = "gray", col = "blue")
+
+# Read in and plot climate layer and country basemap
+# Climate: bioclim layer 1, 2.5 min, generic
+bio1c = raster("bioclim_current/bio1.bil")
+plot(bio1c, col = grey(0:256/256), legend = FALSE, main = "Global Current Temp")
+plot(world, border = "blue", col = "transparent", add = TRUE)
+
+# Read in entire cleaned Vertnet dataset with body size
+all_occurrences = read.csv("CompleteDatasetVN.csv")
+
+# Pick one species with large number of occurrences
+# Sorex trowbridgii (Trowbridge's shrew): range is along west coast of N America, mass 3.8-5g
+shrew = all_occurrences[all_occurrences$clean_genus_species == "Sorex trowbridgii",]
+# Put into spatial format, just longitude and latitude cols
+shrew_sp = SpatialPointsDataFrame(shrew[, 19:18], shrew, proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+points(shrew_sp, col = "red")
+
+# Not clipping climate layers because don't know how much of them will be necessary
+# Climate: bioclim layers 1 and 4
+bioclim_list = list.files(path = "bioclim_current/", full.names = TRUE, pattern = ".bil")
+climate_layers = stack(bioclim_list)
+plot(climate_layers)
+
+# Visualizing climate layers
+library(rasterVis)
+histogram(climate_layers)
+densityplot(climate_layers)
+levelplot(climate_layers)
+
+# Get climate values for all occurrences
+vals = extract(climate_layers, shrew_sp, method = "bilinear")
+
